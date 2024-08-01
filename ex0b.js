8be9f0f007b225dba1af3dfa45664b4e5d634d0d
dnlslcd <danielsalcedoq@gmail.com>
2024-08-01 21:03:23 +0200

    ex0b - hecho

--- ex0b.js
+++ ex0b.js
@@ -1,8 +1,10 @@
 /**
- * Existe otra forma de crear un array vacio mediante la creación de un nuevo objeto de la clase Array. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array . Cambia la inicialización de la varible 'numeros' y comprueba que el script sigue funcionando igual
+ * Existe otra forma de crear un array vacio mediante la creación de un nuevo objeto 
+ * de la clase Array. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array . 
+ * Cambia la inicialización de la varible 'numeros' y comprueba que el script sigue funcionando igual
  */
 
-const numeros = []; // <-- SOLAMENTE CAMBIAR ESTA LÍNEA
+let numeros = []; // <-- SOLAMENTE CAMBIAR ESTA LÍNEA
 numeros.push(10, 20, 30, 40, 50);
 
 const resultado = numeros.reduce((prev, acc) => prev + acc, 0);
